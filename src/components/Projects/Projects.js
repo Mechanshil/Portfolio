1d4437b2b5fa0ad5b94d63d6a6745be760daef50
InnocentBoy-007 <mechanshil7@gmail.com>
2025-02-07 21:36:05 +0530

    Changed the project lists

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -31,62 +31,58 @@ function Projects() {
                             demoLink="https://travel-with-innocentboy.netlify.app/"
                         />
                     </Col>
-
                     <Col md={4} className="project-card">
                         <ProjectCard
-                            imgPath={bitsOfCode}
+                            imgPath={travelwithus}
                             isBlog={false}
-                            title="Bits-0f-C0de"
-                            description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-                            ghLink=""
-                            demoLink=""
+                            title="Travel With Us"
+                            description="A website where all the travel enthusiasts can come together and enjoy their moments. It also provides services and guides to the travelers."
+                            ghLink="https://github.com/InnocentBoy-007/travelwithus.git"
+                            demoLink="https://travel-with-innocentboy.netlify.app/"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={travelwithus}
+                            isBlog={false}
+                            title="Travel With Us"
+                            description="A website where all the travel enthusiasts can come together and enjoy their moments. It also provides services and guides to the travelers."
+                            ghLink="https://github.com/InnocentBoy-007/travelwithus.git"
+                            demoLink="https://travel-with-innocentboy.netlify.app/"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={travelwithus}
+                            isBlog={false}
+                            title="Travel With Us"
+                            description="A website where all the travel enthusiasts can come together and enjoy their moments. It also provides services and guides to the travelers."
+                            ghLink="https://github.com/InnocentBoy-007/travelwithus.git"
+                            demoLink="https://travel-with-innocentboy.netlify.app/"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={travelwithus}
+                            isBlog={false}
+                            title="Travel With Us"
+                            description="A website where all the travel enthusiasts can come together and enjoy their moments. It also provides services and guides to the travelers."
+                            ghLink="https://github.com/InnocentBoy-007/travelwithus.git"
+                            demoLink="https://travel-with-innocentboy.netlify.app/"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={travelwithus}
+                            isBlog={false}
+                            title="Travel With Us"
+                            description="A website where all the travel enthusiasts can come together and enjoy their moments. It also provides services and guides to the travelers."
+                            ghLink="https://github.com/InnocentBoy-007/travelwithus.git"
+                            demoLink="https://travel-with-innocentboy.netlify.app/"
                         />
                     </Col>
 
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={editor}
-                            isBlog={false}
-                            title="Editor.io"
-                            description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-                            ghLink=""
-                            demoLink=""
-                        />
-                    </Col>
 
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={leaf}
-                            isBlog={false}
-                            title="Plant AI"
-                            description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-                            ghLink=""
-                            demoLink=""
-                        />
-                    </Col>
-
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={suicide}
-                            isBlog={false}
-                            title="Ai For Social Good"
-                            description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-                            ghLink=""
-                            demoLink=""
-                        />
-                    </Col>
-
-                    <Col md={4} className="project-card">
-                        <ProjectCard
-                            imgPath={emotion}
-                            isBlog={false}
-                            title="Face Recognition and Emotion Detection"
-                            description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-                            ghLink=""
-                            demoLink=""
-                        />
-                    </Col>
                 </Row>
             </Container>
         </Container>
